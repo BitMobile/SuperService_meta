02c62250bfe5737882325ef91c1941debfdebc98
Alexandr Nemcov <alexnemcov@yandex.ru>
2016-11-07 18:47:08 +0300

    Добавлен скрипт обновления на новую БД

---
+++ UpdateScripts/0003-3.1.4.0.sql
@@ -0,0 +1,78 @@
+--
+-- DO NOT START SCRIPT FOR INCORRECT VERSION
+-- 
+
+
+IF NOT EXISTS(SELECT *  FROM  [dbo].[dbConfig] WHERE [Key]='DBVersion' AND [Value]='3.1.3.1' )
+
+  RETURN
+
+--
+-- DO NOT START SCRIPT FOR CURRENT VERSION
+-- 
+
+IF EXISTS(SELECT *  FROM  [dbo].[dbConfig] WHERE [Key]='DBVersion' AND [Value]='3.1.4.0' )
+
+  RETURN
+
+SET NUMERIC_ROUNDABORT OFF
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+SET XACT_ABORT ON
+GO
+SET TRANSACTION ISOLATION LEVEL Serializable
+GO
+BEGIN TRANSACTION
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+
+--
+-- INSERT ALL MODIFICATIONS HERE 
+--
+
+
+
+--
+-- BEGIN DATABASE VERSION SETUP 
+--
+
+PRINT N'Set DBVersion = 3.1.4.0'
+
+IF EXISTS(SELECT *  FROM  [dbo].[dbConfig] WHERE [Key]='DBVersion')
+    UPDATE [dbo].[dbConfig]  
+      SET [Value]='3.1.4.0'  
+      WHERE [Key]='DBVersion'; 
+  ELSE
+    INSERT INTO [dbo].[dbConfig]
+           ([Key]
+           ,[Value])
+       VALUES
+           ('DBVersion'
+           ,N'3.1.4.0');
+GO
+
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+
+--
+-- END DATABASE VERSION SETUP 
+--
+
+
+COMMIT TRANSACTION
+GO
+
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+
+DECLARE @Success AS BIT 
+SET @Success = 1 
+SET NOEXEC OFF 
+IF (@Success = 1) PRINT 'The database update succeeded' 
+ELSE BEGIN 
+  IF @@TRANCOUNT > 0 ROLLBACK TRANSACTION 
+  PRINT 'The database update failed' 
+END
+GO
